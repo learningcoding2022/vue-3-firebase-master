e6e5486e25e3c26b3abf1870ab3bb8b4eb8ed458
Molly <117697419+learningcoding2022@users.noreply.github.com>
2024-05-19 09:42:11 -0400

    update files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
-    //data, functions, template
-    template: <h2>I am the template</h2>
-})
-
-//tells the computer where to mount it
-app.mount('#app')
+    // data, functions etc
+    // template: '<h2>I am the template</h2>',
+  })
+  
+  app.mount('#app')
