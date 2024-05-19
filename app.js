e838252c3c696fb386a384f1104eef9d63c6a114
Molly <117697419+learningcoding2022@users.noreply.github.com>
2024-05-19 09:58:46 -0400

    dynamic data: title, author, age

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const app = Vue.createApp({
-    // data, functions etc
+    // data, functions template (template can get messy here so you should put it in html)
     // template: '<h2>I am the template</h2>',
-  })
+    
+    //function
+    data() {
+        //return object
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
+
+    })
   
   app.mount('#app')
